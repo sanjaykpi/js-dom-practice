217fba38620c0172e75bd82d9656650507cc9b92
Sanjay Selvamani <sanjayselvamani6@gmail.com>
2022-08-24 16:42:25 -0700

    dom-pra

--- dom-practice/script.js
+++ dom-practice/script.js
@@ -1,11 +1,22 @@
-// java script Id method
+//  java script Id method
 
-const a=document.getElementById("start");
+// const a=document.getElementById("start");
 
-document.getElementById("demo").innerHTML="web page user friendly for " + a.innerHTML;
+// document.getElementById("demo").innerHTML="web page user friendly for " + a.innerHTML;
 
-// java script class method
+//  java script class method
 
-const collection = document.getElementsByClassName("phone");
+// const collection = document.getElementsByClassName("phone");
 
-collection[1].innerHTML = "one plus!";
+// collection[1].innerHTML = "one plus!";
+
+function check(){
+    let password=document.getElementById("password").value;
+    let password2=document.getElementById("conform-password").value;
+    if(password==password2){
+        document.getElementById("error").innerHTML="match your password"
+    }
+    else{
+        document.getElementById("error").innerHTML="did not match your password"
+    }
+}
