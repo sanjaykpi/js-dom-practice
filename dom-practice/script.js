31fce3344450082ed79d668f7c962fd5470452c5
Sanjay Selvamani <sanjayselvamani6@gmail.com>
2022-08-23 20:52:26 -0700

    dom-pra

---
+++ dom-practice/script.js
@@ -0,0 +1,11 @@
+// java script Id method
+
+const a=document.getElementById("start");
+
+document.getElementById("demo").innerHTML="web page user friendly for " + a.innerHTML;
+
+// java script class method
+
+const collection = document.getElementsByClassName("phone");
+
+collection[1].innerHTML = "one plus!";
