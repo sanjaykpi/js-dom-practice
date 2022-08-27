f0fffc9b22cc220e7db6d96172c5ef8aa3dcdebb
Sanjay Selvamani <sanjayselvamani6@gmail.com>
2022-08-27 11:19:38 -0700

    dom-pro

--- dom-cart/script.js
+++ dom-cart/script.js
@@ -1,9 +1,25 @@
-  function product(){
-  let unit=document.getElementById("unitprice").innerText;
-  let qty=document.getElementById("quentity").value;
-  let result =unit*qty;
-  let price=document.getElementById("price").innerHTML=result;
+//   function product(){
+//   let unit=document.getElementById("unitprice").innerText;
+//   let qty=document.getElementById("quentity").value;
+//   let result =unit*qty;
+//   let price=doument.getElementById("price").innerHTML=result;
+// }
+
+// let button=document.getElementById("trigger").addEventListener("click",product);
+ function calculate(){
+  let a=this.parentNode.parentNode;
+  // console.log(a);
+
+  let unit=a.getElementsByClassName("unitprice")[0].innerText;
+  let qty=a.getElementsByClassName("quentity")[0].value;
+  let priceele=a.getElementsByClassName("price")[0];
+  unit=parseInt(unit);
+  let result=unit*qty;
+  priceele.innerHTML=result; 
+
 }
+ let checkele=document.getElementsByClassName("trigger");
 
-let button=document.getElementById("trigger").addEventListener("click",product);
-
+ for(i=0;i<checkele.length;i++){
+    checkele[i].addEventListener("click",calculate);
+ }
